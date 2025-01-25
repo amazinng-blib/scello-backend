4e687291f59cf2b28eb35268b4e1eb5c2c5c226e
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2025-01-25 10:44:02 +0100

    installed jwt and add jwt verify token

---
+++ src/migrations/verifyToken.ts
@@ -0,0 +1,32 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void => {
+  try {
+    let token = req.header('Authorization');
+    if (!token) {
+      res.status(401).json({ message: 'Access denied' });
+      return;
+    }
+
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length);
+    }
+
+    const verified = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET as Secret
+    ) as JwtPayload;
+    req.userId = verified.id;
+    next();
+  } catch (error: any) {
+    console.log('Token verification error', error.message);
+    res.status(401).json({ message: 'Some error occured' });
+  }
+};
