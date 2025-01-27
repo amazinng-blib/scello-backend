9219beff22c3a0b90aab8f96dbcf9fc41b1ffb68
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2025-01-27 16:39:34 +0100

    add updateProduct schema

--- src/validation/productSchema.ts
+++ src/validation/productSchema.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
 export const productSchema = z.object({
+  id: z.number().int().positive().optional(),
   name: z.string().min(2, 'Product name must be at least 2 characters'),
+  creator: z.number().positive(),
   price: z.number().positive('Price must be a positive number'),
   description: z.string().min(5, 'Description must be at least 5 characters'),
   stockQuantity: z
@@ -12,3 +14,6 @@ export const productSchema = z.object({
 });
 
 export type productType = z.infer<typeof productSchema>;
+export const updateProductSchema = productSchema.partial();
+
+export type updateProductType = z.infer<typeof updateProductSchema>;
